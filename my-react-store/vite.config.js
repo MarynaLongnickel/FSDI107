72142feda3563be37b3c72c50671c36d8451525c
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-07-26 15:57:15 -0500

    Updates

--- my-react-store/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/FSDI107/my-react-store/',
-  plugins: [react()],
-})
-
