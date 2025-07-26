9c558134d06fa6ac3ffe87562da817750ef8a58e
Maryna Longnickel <maryna.longnickel@gmail.com>
2025-07-26 16:01:06 -0500

    wip

--- my-react-store/vite.config.js
+++ my-react-store/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/FSDI107/',
+  base: '/FSDI107/my-react-store/',
   plugins: [react()],
 })
 
